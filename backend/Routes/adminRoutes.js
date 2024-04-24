e49cdc590cc0b0d343f16b1a805e7766d3c6a51d
Aryan Sharma <95480865+AryanSharma197@users.noreply.github.com>
2024-04-24 23:43:26 +0530

    Commit

--- backend/Routes/adminRoutes.js
+++ backend/Routes/adminRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { createAdmin, adminLogin, updatedPassword, updateAdmin, addAdmin, getAllAdmin, deleteAdmin } = require("../Controller/adminController");
 const router = express.Router();
 
+//ADMIN ROUTES
 router.post("/register", createAdmin);
 router.post("/login", adminLogin);
 router.post("/updatepassword", updatedPassword);
@@ -9,5 +10,7 @@ router.post("/updateprofile", updateAdmin);
 router.post("/addadmin", addAdmin);
 router.get("/getalladmin", getAllAdmin);
 router.delete("/deleteadmin/:id", deleteAdmin);
+
+//DEPARTMENT ROUTES
 
 module.exports = router
